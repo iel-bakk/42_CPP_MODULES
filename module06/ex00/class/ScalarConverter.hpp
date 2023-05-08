7457e82e1a65ca14b1fba786ac337e88d20100f8
ismail el bakkouchi <iel-bakk@e1r3p7.1337.ma>
2023-05-08 01:04:55 +0100

    stash

--- module06/ex00/class/ScalarConverter.hpp
+++ module06/ex00/class/ScalarConverter.hpp
@@ -6,32 +6,35 @@
 /*   By: iel-bakk <iel-bakk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:36:57 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/05/08 00:45:37 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/05/08 01:02:27 by iel-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <string>
+
 class ScalarConverter
 {
-private:
-    static char    _char;
-    static int     _int;
-    static float   _float;
-    static double  _double;
+// private:
+//     static char    _char;
+//     static int     _int;
+//     static float   _float;
+//     static double  _double;
 public:
-    ScalarConverter();
-    ~ScalarConverter();
-    void    set_char(char c);
-    void    set_int(int number);
-    void    set_float(float number);
-    void    set_double(float number);
-    void    print_char_value();
-    void    print_int_value();
-    void    print_float_value();
-    void    print_double_value();
+    // ScalarConverter();
+    // ~ScalarConverter();
+    static void    converter(std::string param);
+    // static void    set_char(char c);
+    // static void    set_int(int number);
+    // static void    set_float(float number);
+    // static void    set_double(float number);
+    static void    print_char_value(char c);
+    static void    print_int_value(int number);
+    static void    print_float_value(float number);
+    static void    print_double_value(double number);
 };
-
 
 #endif
--- module06/ex00/src/ScalarConverter.cpp
+++ module06/ex00/src/ScalarConverter.cpp
@@ -6,16 +6,42 @@
 /*   By: iel-bakk <iel-bakk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:38:36 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/05/08 00:40:33 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/05/08 01:04:19 by iel-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../class/ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter() {
-    this->_char = 0;
-    this->_char = 0;
-    this->_char = 0;
-    this->_double = 0;
+// ScalarConverter::ScalarConverter() {
+//     this->_char = 0;
+//     this->_char = 0;
+//     this->_char = 0;
+//     this->_double = 0;
+// }
+
+void    ScalarConverter::converter(std::string param) {
+    try {
+    double number = std::stod(param);
+    // ScalarConverter::set_char(static_cast<char>number);
+    // ScalarConverter::set_int(static_cast<int>number);
+    // ScalarConverter::set_float(static_cast<float>number);
+    // ScalarConverter::set_double(static_cast<double>number);
+    ScalarConverter::print_char_value(static_cast<char>number);
+    ScalarConverter::print_int_value(static_cast<int>number);
+    ScalarConverter::print_float_value(static_cast<float>number);
+    ScalarConverter::print_double_value(static_cast<double>number);
+    } catch (const std::invalid_argument& e) {
+        std::cout << "Invalid argument: " << e.what() << std::endl;
+    } catch (const std::out_of_range& e) {
+        std::cout << "Out of range: " << e.what() << std::endl;
+    }
 }
 
+void    ScalarConverter::set_char(char c) {
+    if (c < 32 || c > 127)
+        std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char : " + c << std::endl;
+}
+
+void    
