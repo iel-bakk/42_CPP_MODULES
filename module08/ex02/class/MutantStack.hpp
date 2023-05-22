8ffe3bfbf2d946e04ace1efb1f335f6e74133b19
ismail el bakkouchi <iel-bakk@e1r4p4.1337.ma>
2023-05-22 02:07:43 +0100

    finished cpp module 08

--- module08/ex02/class/MutantStack.hpp
+++ module08/ex02/class/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: iel-bakk <iel-bakk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 01:07:19 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/05/22 01:34:40 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/05/22 02:07:12 by iel-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,19 @@
 #include <stack>
 #include <deque>
 
-template <typename T, class container = std::deque<T>>
-class MutantStack : public std::stack<T> {
+template <typename T, class container = std::deque < T > >
+class MutantStack : public std::stack<T, container> {
     public :
         MutantStack() {};
         ~MutantStack() {};
-        
+        MutantStack(MutantStack& obj) {*this = obj;}
+        MutantStack& operator=(MutantStack& obj) {
+            std::stack<T, container>::operator=(obj);
+            return (*this);
+        }
+        typedef typename container::iterator iterator;
+        iterator begin() {return this->c.begin();}
+        iterator end() {return this->c.end();}
 };
 
 #endif
