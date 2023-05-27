6bfe1c8644f9fe4def92678e2b44624ccc383448
ismail el bakkouchi <iel-bakk@e1r3p7.1337.ma>
2023-05-27 22:43:42 +0100

    update code

--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iel-bakk <iel-bakk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 20:32:17 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/05/27 21:13:21 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/05/27 22:41:21 by iel-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int ac, char **av) {
     int first;
     int second;
     std::stack<int> numbers;
-    char    op = '0';
+    std::stack<int> tmp;
 
     if (ac != 2) {
         std::cout << "Error : invalid arguments." << std::endl;
@@ -28,26 +28,26 @@ int main(int ac, char **av) {
     for (int i = 0; av[1][i] != '\0'; i++) {
         if (av[1][i] != ' ') {
             if (av[1][i] >= '0' && av[1][i] <= '9') {
-                numbers.push(atoi(av[1] + i));
-                if (op != '0') {
-                    first = numbers.top();
-                    numbers.pop();
-                    second = numbers.top();
-                    numbers.pop();
-                    numbers.push(calculate(first, second, op));
-                    op = '0';
-                }
+                    numbers.push(atoi(av[1] + i));
             }
             else if ((av[1][i] == '+' || av[1][i] == '-' || av[1][i] == '/' || av[1][i] == '*')) {
-                if (numbers.size() >= 2) {
+                if (numbers.size() > 2) {
+                    while (numbers.size() > 2) {
+                        tmp.push(numbers.top());
+                        numbers.pop();
+                    }
+                }
                     first = numbers.top();
                     numbers.pop();
                     second = numbers.top();
                     numbers.pop();
-                    numbers.push(calculate(first, second, av[1][i]));
+                    numbers.push(calculate(second, first, av[1][i]));
+                if (tmp.size() > 0) {
+                    while (tmp.size() > 0) {
+                        numbers.push(tmp.top());
+                        tmp.pop();
+                    }
                 }
-                else
-                    op = av[1][i];
             }
             else {
                 std::cout << "Error." << std::endl;
--- module09/ex01/src/RPN.cpp
+++ module09/ex01/src/RPN.cpp
@@ -6,13 +6,14 @@
 /*   By: iel-bakk <iel-bakk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 20:53:32 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/05/27 21:05:17 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/05/27 22:36:51 by iel-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../class/RPN.hpp"
 
 int calculate(int a, int b, char op) {
+    std::cout << "first : " << a << " second : " << b << " op : " << op << std::endl;
     switch (op)
     {
     case '+' :
